f546dba480644c109f17dd36cb8822d1bff02c99
jaikaransingh <76830551+jaikaransingh@users.noreply.github.com>
2021-04-29 13:21:27 +0530

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-center">
+        <div class="col-md-8">
+            <h2>Welcome</h2>
+        </div>
+    </div>
+</div>
+
+@endsection
